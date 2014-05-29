e95ee36c32b73bc295125e96f62576994b59da46
jia58960 <jia58960@163.com>
2014-05-29 14:48:15 +0800

    引入markdown

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
-var mongodb = require('./db');
+var mongodb = require('./db'),
+    markdown = require('markdown').markdown;
 
 function Post (name, title, post) {
 	this.name = name;
@@ -13,7 +14,7 @@ Post.prototype.save = function (callback) {
 
 	//存储各种时间格式，后用
 	var time = {
-  		date: date,
+  		  date: date,
       	year : date.getFullYear(),
       	month : date.getFullYear() + "-" + (date.getMonth() + 1),
       	day : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
@@ -77,6 +78,10 @@ Post.get = function(name, callback) {
         if (err) {
           return callback(err);//失败！返回 err
         }
+        //解析 markdown 为 html
+        docs.forEach(function (doc) {
+          doc.post = markdown.toHTML(doc.post);
+        });
         callback(null, docs);//成功！以数组形式返回查询的结果
       });
     });
